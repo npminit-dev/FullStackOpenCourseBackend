a3f14bcd8d00f127076fbcf18185a4447f3bfbc3
Jorge Antonio Balsamo <jorgeb.dev.acc@gmail.com>
2023-11-11 13:55:37 -0300

    Min changes

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 import express, { json } from "express";
 import morgan from "morgan";
 import { persons as p } from "./persons.js";
+import cors from 'cors'
 
 let persons = [...p];
 
 const app = express();
 
 app.use(morgan((tokens, req, res) => {
-  console.log(res)
   return [
     tokens.method(req, res),
     tokens.url(req, res),
@@ -16,6 +16,7 @@ app.use(morgan((tokens, req, res) => {
   ].join(' - ')
 }, { immediate: false }));
 app.use(json());
+app.use(cors())
 
 app.get('/info', (req, res) => {
   res.statusMessage = 'Correct!';
@@ -34,6 +35,7 @@ app.get('/api/persons/:id', (req, res) => {
 })
 
 app.delete('/api/persons', (req, res) => {
+  console.log('body: ', req.body)
   let data = req.body;
   if(data && data.id) {
     let prevLength = persons.length;
@@ -48,11 +50,12 @@ app.post('/api/persons', (req, res) => {
   if(data) {
     if(!data.name || !data.number) return res.status(400).send('Missing properties!');
     if(persons.find(person => data.name === person.name)) return res.status(400).send('Name already exists')
-    persons.push({ id: Math.round(Math.random() * 1000000), ...data });
-    return res.status(200).send(persons);
+    let newPerson = { id: Math.round(Math.random() * 1000000), ...data }
+    persons.push(newPerson);
+    return res.status(200).json(newPerson);
   } else res.status(400).send('No body!');
 })
 
-app.listen(3001, () => console.log('Server listening at port 3001'))
+app.listen(process.env.PORT || 3001, () => console.log('Server listening at port 3001'))
 
 
--- persons.js
+++ persons.js
@@ -1,21 +1,21 @@
 export let persons = [
   {
-    id: 1,
+    id: 345111,
     name: "Arta Lovelace",
     number: "+541122336677"
   },
   {
-    id: 2,
+    id: 999654,
     name: "Michael Jackson",
     number: "+541156743211"
   },
   {
-    id: 3,
+    id: 191922,
     name: "Nicholas Faitelson",
     number: "11-345-1199"
   },
   {
-    id: 4,
+    id: 119211,
     name: "Linda RavenClaw",
     number: "111-222-333-444"
   }
