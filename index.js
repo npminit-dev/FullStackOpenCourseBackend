5a951592eff9ad37ff49d0cca314cfb883608d37
Jorge Antonio Balsamo <jorgeb.dev.acc@gmail.com>
2023-11-11 12:02:04 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,58 @@
+import express, { json } from "express";
+import morgan from "morgan";
+import { persons as p } from "./persons.js";
+
+let persons = [...p];
+
+const app = express();
+
+app.use(morgan((tokens, req, res) => {
+  console.log(res)
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    'POST body: ' + (JSON.stringify(req.body))
+  ].join(' - ')
+}, { immediate: false }));
+app.use(json());
+
+app.get('/info', (req, res) => {
+  res.statusMessage = 'Correct!';
+  res.status(200).send(new Date().toString());
+})
+
+app.get('/api/persons', (req, res) => {
+  res.statusMessage = 'Correct!';
+  res.status(200).send(persons);
+})
+
+app.get('/api/persons/:id', (req, res) => {
+  let person = persons.find(person => person.id.toString() === req.params.id);
+  if(person) res.status(200).send(person);
+  else res.status(404);
+})
+
+app.delete('/api/persons', (req, res) => {
+  let data = req.body;
+  if(data && data.id) {
+    let prevLength = persons.length;
+    persons = persons.filter(person => person.id != data.id);
+    if(prevLength === persons.length + 1) res.status(200).send(persons);
+    else res.status(400).send('Incorrect ID!');
+  } else res.status(404).send('No body or incorrect body!');
+})
+
+app.post('/api/persons', (req, res) => {
+  let data = req.body;
+  if(data) {
+    if(!data.name || !data.number) return res.status(400).send('Missing properties!');
+    if(persons.find(person => data.name === person.name)) return res.status(400).send('Name already exists')
+    persons.push({ id: Math.round(Math.random() * 1000000), ...data });
+    return res.status(200).send(persons);
+  } else res.status(400).send('No body!');
+})
+
+app.listen(3001, () => console.log('Server listening at port 3001'))
+
+
---
+++ persons.js
@@ -0,0 +1,22 @@
+export let persons = [
+  {
+    id: 1,
+    name: "Arta Lovelace",
+    number: "+541122336677"
+  },
+  {
+    id: 2,
+    name: "Michael Jackson",
+    number: "+541156743211"
+  },
+  {
+    id: 3,
+    name: "Nicholas Faitelson",
+    number: "11-345-1199"
+  },
+  {
+    id: 4,
+    name: "Linda RavenClaw",
+    number: "111-222-333-444"
+  }
+]
